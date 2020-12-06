5c84739a140cf13fad18626b34e2267a47903ef6
Sulivan <sulivan.lineage2@gmail.com>
2020-12-06 12:03:26 -0300

    configuraçao de dependencias e primeiros testes

--- src/test/java/com/wallet/HelloWorldTest.java
+++ src/test/java/com/wallet/HelloWorldTest.java
@@ -3,10 +3,13 @@ package com.wallet;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-public class HelloWorld {
+public class HelloWorldTest {
 
 	@Test
 	public void testHelloWorld() {
