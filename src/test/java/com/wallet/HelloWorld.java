69f5bacd82671c713d5eaa004d3886b51f4fbeb9
Sulivan <sulivan.lineage2@gmail.com>
2020-12-06 11:29:30 -0300

    primeiro commit

---
+++ src/test/java/com/wallet/HelloWorld.java
@@ -0,0 +1,18 @@
+package com.wallet;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class HelloWorld {
+
+	@Test
+	public void testHelloWorld() {
+
+		assertEquals(1, 1);
+
+	}
+
+}
